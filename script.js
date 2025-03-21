e715006e18952815ab3ccb657e4c3aaa70497971
Kevin Isay Guerrero <kevisaycortes@gmail.com>
2025-03-21 08:33:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ const redCar = new Image();
 redCar.src = "redCar.png"; 
 
 const azulCar = new Image();
-azulCar.src = "azulCar.png"
+azulCar.src = "azulCar.png";
 
 const draw = () => {
     ctx.clearRect(0, 0, anchoCanvas, altoCanvas);
@@ -67,16 +67,11 @@ const draw = () => {
     
 };
 
+redCar.onerror = () => console.error("Error al cargar la imagen redCar");
+azulCar.onerror = () => console.error("Error al cargar la imagen azulCar");
+
 redCar.onload = () => draw();
 azulCar.onload = () => draw();
-
-redCar.onerror = () => {
-    console.error("Error al cargar la imagen redCar");
-};
-
-azulCar.onerror = () => {
-    console.error("Error al cargar la imagen azulCar");
-};
 
 //mover Pj
 let mover = false;
